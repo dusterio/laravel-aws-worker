97d6954f6ac6cff902fb6b3ef25484d93182c824
Benno Eggnauer <benno@eggnauer.com>
2017-10-31 14:26:30 +0100

    Laravel 5.5 support.

--- src/Integrations/BindsWorker.php
+++ src/Integrations/BindsWorker.php
@@ -16,7 +16,7 @@ trait BindsWorker
      * @var array
      */
     protected $workerImplementations = [
-        '5\.[34].*' => Laravel53Worker::class
+        '5\.[345].*' => Laravel53Worker::class
     ];
 
     /**
