cbca1cc6d98ca0f9b1ede6ad60643cc268d993e9
Neil Aldred <neil@cyber-duck.co.uk>
2023-03-16 01:24:25 +0000

    Convert app version to int before confirming. Its currently doing a string comparison meaning "9.0.0" is greater than 6 but "10.0.0" is not as it begins with a 1. Converting to int changes "10.0.0" to 10 allowing it to pass the condition correctly

--- src/Integrations/BindsWorker.php
+++ src/Integrations/BindsWorker.php
@@ -44,7 +44,7 @@ trait BindsWorker
     protected function bindWorker()
     {
         // If Laravel version is 6 or above then the worker bindings change. So we initiate it here
-        if ($this->app->version() >= 6) {
+        if ((int) $this->app->version() >= 6) {
             $this->app->singleton(Worker::class, function () {
                 $isDownForMaintenance = function () {
                     return $this->app->isDownForMaintenance();
